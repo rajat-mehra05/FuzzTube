2792deafedb4562c65e63fc1313475d59a3ba40a
RAJAT MEHRA <rajat.m.5278@gmail.com>
2021-06-03 20:17:52 +0530

    removed api keys from config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCosoL0Fdg0uqge8-7HpjZMPN4avsTZLqs",
+  apiKey: "",
   authDomain: "fuzztube-72e3f.firebaseapp.com",
   projectId: "fuzztube-72e3f",
   storageBucket: "fuzztube-72e3f.appspot.com",
-  messagingSenderId: "640308302112",
-  appId: "1:640308302112:web:1ddb37606c42b89e7ac745",
+  messagingSenderId: "",
+  appId: "1::web:",
 };
 //provide your own config from firebase
 firebase.initializeApp(firebaseConfig);
